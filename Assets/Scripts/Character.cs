c99ef0fa2191c01d9d58c37e3d6edaf26a929cd6
BRousserie <32737696+BRousserie@users.noreply.github.com>
2019-01-25 22:24:27 +0100

    Resources

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private Resource[] m_resources = new Resource[Resource.NB_RESOURCE_TYPES];
+    public Resource[] Resources { get { return m_resources; } }
+
+    public void setResources(ResourceType type, int value)
+    {
+        m_resources[(int)type].Value = value;
+    }
+
+    /**
+     * Used to set the whole resources' array 
+     */
+    public void setResources(Resource[] resources)
+    {
+        if (resources.Length == Resource.NB_RESOURCE_TYPES)
+            m_resources = resources;
+        else
+            throw new System.Exception(
+                "Assigning " + resources.Length + " resources " +
+                " but " + Resource.NB_RESOURCE_TYPES +
+                " different resource types exist (when calling Character.setResources())");
+    }
+
+    /**
+     * Used to set one type of resources
+     */
+    public void setResources(Resource resource)
+    {
+        m_resources[(int)resource.Type].Value = resource.Value;
+    }
+
+    /**
+     * Used when the player recieves or loses resources 
+     * resource.Value is negative when losing resources
+     */
+    public void addResources(Resource resource)
+    {
+        m_resources[(int)resource.Type].Value += resource.Value;
+    }
+}
---
+++ Assets/Scripts/Resources.cs
@@ -0,0 +1,11 @@
+﻿public enum ResourceType { Wood = 0 }; //Si l'on ajoute un nouveau type, il faut mettre à jour NB_RESOURCE_TYPES
+
+public class Resource
+{
+    public const int NB_RESOURCE_TYPES = 1;
+
+    private ResourceType m_type;
+    public ResourceType Type { get { return m_type; } }
+    private int m_value;
+    public int Value { get { return m_value; } set { m_value = value; } }
+}
