65883e9c331c6f0b92b4ff0351e58a8963e28240
MSI\Nico <lepilolodior7633@gmail.com>
2019-01-26 10:18:23 +0100

    maj batiment

---
+++ Assets/Scripts/Batiment.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Batiment : MonoBehaviour
+{
+    Camera cam;
+    bool readyClick = true;
+    //GameObject cube;
+    void Start()
+    {
+        cam = Camera.main;
+        //cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        //cube.transform.localScale = new Vector3(0.1f, 0.3f, 0.1f);
+
+        //wfs = new WaitForSeconds(0.1f);
+
+        //StartCoroutine(test());
+    }
+
+    WaitForSeconds wfs;
+
+    /*IEnumerator test()
+    {
+
+        while (true)
+        {
+            OnMouseUp();
+            yield return wfs;
+        }
+    }*/
+
+    void OnMouseUp ()
+    {
+        RaycastHit hit;
+        if (!Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit))
+            return;
+
+        MeshCollider meshCollider = hit.collider as MeshCollider;
+        if (meshCollider == null || meshCollider.sharedMesh == null)
+            return;
+
+        Mesh mesh = meshCollider.sharedMesh;
+        Vector3[] vertices = mesh.vertices;
+        Vector3[] normals = mesh.normals;
+        int[] triangles = mesh.triangles;
+        Vector3 p0 = vertices[triangles[hit.triangleIndex * 3]];
+        Vector3 normal = normals[triangles[hit.triangleIndex * 3]];
+        Transform hitTransform = hit.collider.transform;
+        
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.position = p0;
+        cube.transform.localScale = new Vector3(0.1f, 0.3f, 0.1f);
+        cube.transform.rotation = Quaternion.FromToRotation(transform.up, normal) * transform.rotation;
+    }
+}
