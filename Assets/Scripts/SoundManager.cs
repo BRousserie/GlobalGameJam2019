9d26200a2e110dc380d06fde283e6ef47d1bc34c
wil20cts <wil20cts@gmail.com>
2019-01-27 10:49:49 +0100

    Squelette SoundManager

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+	public static SoundManager instance;
+	public SoundItem [] soundItems;
+
+	void Awake(){
+		if(instance == null){
+			DontDestroyOnLoad(gameObject);
+			instance = this;
+		}else if(instance != this){
+			Destroy(gameObject);
+		}
+	}
+
+
+    // Start is called before the first frame update
+    void PlaySound(string soundName)
+    {
+        
+    }
+}
+
+[System.serialized]
+public class SoundItem
+{
+	public string soundName;
+	public AudioSource audioSource;
+}
